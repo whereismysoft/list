d6471bdeb329510562ad7860b89e0442128f6f09
whereismysoft <kotovyuriy27@gmail.com>
2019-03-20 17:08:55 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    // webpack 4 takes '/src/index.js' by default
+    // entry: path.join(__dirname, '/src/index.js'),
+
+    // output file by default is './dist/main.js'
+    output: {
+        publicPath: "/"
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                          modules: true,
+                          localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                        },
+                    },
+                ]
+            },
+        ]
+    },
+    devServer: {
+        inline: true,
+        hot: true,
+        publicPath: '/',
+        port: 9000,
+        historyApiFallback: true,
+        watchContentBase: true
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '/src/index.html'),
+            inject: 'body'
+        })
+    ]
+ }
